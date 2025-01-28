7fe69cf9a3d17f8c903dc21dd952faedb6378445
Yu-Pang <tedchen524@gmail.com>
2025-01-28 17:14:37 +0800

    ✨ Update render function

---
+++ cmd/web/templates.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/AlliesChen/snippetbox-go/internal/models"
+)
+
+type templateData struct {
+	Snippet  models.Snippet
+	Snippets []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add the template set to the map
+		cache[name] = ts
+	}
+	return cache, nil
+}
