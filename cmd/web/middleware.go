e461bf5b66363e79132e1eb7ef934eb650a63e76
Yu-Pang <tedchen524@gmail.com>
2025-03-22 23:37:11 +0800

    ✨ CSRF protection

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
 func commonHeaders(next http.Handler) http.Handler {
@@ -57,4 +59,16 @@ func (app *application) requireAuthenticatedUser(next http.Handler) http.Handler
 		w.Header().Add("Cache-Control", "no-store")
 		next.ServeHTTP(w, r)
 	})
+}
+
+// Uses a customized CSRF cookie with the attributes set.
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   true,
+	})
+
+	return csrfHandler
 }
